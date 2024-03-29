6656fde028e822e1c4cd6e15b1552d03bed5784b
karthik <43676649+brkuhgk@users.noreply.github.com>
2024-03-29 17:00:17 -0400

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ const projects = [
     desc: "Group Note is a collaborative note-taking application that allows teams and groups to work together in real-time on shared notes. With Group Note, capturing ideas and collaborating seamlessly becomes a breeze.",
     stack: ["React", "Nodejs", "MangoDB", "Jest"],
     github: "https://github.com/brkuhgk/GroupNotes.git",
-    image: "https://img.icons8.com/ios/50/lifecycle--v1.png",
+    image: "https://i.imgur.com/5wcGYr1.jpeg",
     live: "https://www.youtube.com/watch?v=IKYB2NRoTNc",
   },
   {
@@ -20,23 +20,76 @@ const projects = [
 export default function Projects() {
   return (
     <>
-      <div className="flex items-start justify-center px-4 py-2 bg-white rounded-lg shadow-md bg-lime-500">
-        <ProjectCard projects={projects}/>
-       
+      <div className="flex flex-wrap items-start justify-center px-4 py-2 bg-black rounded-lg shadow-md">
+        <FlipCard projects={projects} />
+        <FlipCard projects={projects} />
+        <FlipCard projects={projects} />
+        <FlipCard projects={projects} />
+        <FlipCard projects={projects} />
+
+
+
       </div>
     </>
   );
 }
 
+/* <div className="py-2 bg-white rounded-lg shadow-md">
+        Project 1
+        <img src={projects[0].image} alt="project logo" className="w-40 h-"/>
+
+
+      </div>
+      <div></div>
+      <div></div> */
+
 function ProjectCard({ projects }) {
   return (
     <>
-      <div className="py-2 bg-white rounded-lg shadow-md">
-        Project 1
-        <img src={projects[0].image} alt="project logo" />
+      <div className="cursor-pointer group perspective">
+        <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+          <div className="absolute backface-hidden w-full h-full">
+            {/* back content */}
+          </div>
+          <div className="absolute my-rotate-y-180 backface-hidden w-full h-full overflow-hidden bg-black">
+            {/* front content */}
+            <img src={projects[0].image} alt="project logo" className="" />
+            hi
+          </div>
+        </div>
       </div>
-      <div></div>
-      <div></div>
+    </>
+  );
+}
+
+function FlipCard({projects}) {
+  return (
+    <>
+    <section class="bg-grey-900 flex justify-center items-center text-white p-4">
+        <div class="w-[300px] h-[400px] bg-transparent cursor-pointer group perspective">
+          <div class="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+            <div class="absolute backface-hidden border-2 w-full h-full">
+              <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
+              <img src= {projects[0].image} class="w-full h-full" />
+
+              </div>
+            </div>
+            <div className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 overflow-hidden">
+              <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
+                <h1 className="text-3xl font-semibold"> {projects[0].name}</h1>
+                <p className="my-2">9.0 Rating</p>
+                <p>
+                  {projects[0].desc}
+                </p>
+                <button className="bg-teal-500 px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125">
+                  Watch Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
@@ -8,5 +28,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [Myclass],
 }
