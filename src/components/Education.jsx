6c5b3c9ec955e2574ebf74a1aa78c4447e493007
karthik <43676649+brkuhgk@users.noreply.github.com>
2024-03-28 21:24:12 -0400

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,95 +1,59 @@
 export default function Education() {
   return (
     <>
-      <div className="flex items-start justify-center p-4">
+      <div className="flex items-start justify-center p-4 font-mono">
         <div className="relative border-s border-gray-200 dark:border-gray-700">
-          <Education2 />
-          <Education2 />
+          <Masters />
+          <Batchlers />
         </div>
       </div>
     </>
   );
 }
 
-{/* <div class="mb-10 ms-4 bg-white rounded-lg shadow-md p-2">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+function Masters() {
+  return (
+    <div class="mb-4 ms-4 bg-white rounded-lg shadow-md p-2">
+      <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+      <div className="text-sm font-normal text-gray-400 dark:text-gray-500">
+        Aug 2022 - May 2024
+      </div>
+      <div className="text-xl font-bold text-gray-900 dark:text-white font">
+        <a href="https://www.montclair.edu"> MONTCLAIR STATE UNIVERSITY ⤴</a>
+      </div>
+      <div className="text-lg font-semibold text-gray-700 dark:text-white">
+        Masters in Computer Science
+      </div>
+      <div className="max-w-md mx-auto text-gray-500">
+        <strong className="text-gray-700">CourseWork : </strong>Computer Algorithms & Analysis, Computer
+        Architecture, Software Engineering, Human-Computer Interactn(HCI), Web
+        Development, Computer Networks, Machine Learning, Robotics
+      </div>
+    </div>
+  );
+}
 
-            <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Aug 2022 - May 2024
-            </div>
-            <div className="text-2xl font-bold text-gray-900 dark:text-white font">
-              <a href="https://www.montclair.edu"> MONTCLAIR STATE UNIVERSITY ⤴</a>
-            </div>
-            <div className="text-lg font-semibold text-gray-700 dark:text-white">
-              Masters in Computer Science
-            </div>
-            <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              <div className="p-2 max-w-md mx-auto">
-                <strong>CourseWork : </strong>Computer Algorithms & Analysis,
-                Computer Architecture, Software Engineering, Human-Computer
-                Interactn(HCI), Web Development, Computer Networks, Machine
-                Learning, Robotics
-              </div>
-            </div>
+function Batchlers() {
+  return (
+    <div class="mb-4 ms-4 bg-white rounded-lg shadow-md p-2">
+      <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
 
-            
-          </div>
-          <div class="mb-10 ms-4">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            Aug 2018 to May 2022
-            </div>
-            <div className="text-2xl font-bold text-gray-900 dark:text-white font">
-             <a href="Velagapudi RamaKrishna Siddhartha College">Velagapudi RamaKrishna Siddhartha College ⤴</a>
-            </div>
-            <div className="text-lg font-semibold text-gray-700 dark:text-white">
-            Batchlers of Engineering 
-            </div>
-            <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              CourseWork : Computer Algorithms & Analysis,
-            </div>
-          </div>
-          <div class="mb-10 ms-4">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              May 2024, Montclair NJ
-            </div>
-            <div className="text-2xl font-bold text-gray-900 dark:text-white font">
-              MONTCLAIR STATE UNIVERSITY
-            </div>
-            <div className="text-lg font-semibold text-gray-700 dark:text-white">
-              Masters in Computer Science
-            </div>
-            <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              CourseWork : Computer Algorithms & Analysis,
-            </div>
-          </div> */}
-
-
-
-function  Education2(){
-return(
-
-    <div class="mb-10 ms-4 bg-white rounded-lg shadow-md p-2">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-
-            <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Aug 2022 - May 2024
-            </div>
-            <div className="text-2xl font-bold text-gray-900 dark:text-white font">
-              <a href="https://www.montclair.edu"> MONTCLAIR STATE UNIVERSITY ⤴</a>
-            </div>
-            <div className="text-lg font-semibold text-gray-700 dark:text-white">
-              Masters in Computer Science
-            </div>
-            <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              <div className="p-2 max-w-md mx-auto">
-                <strong>CourseWork : </strong>Computer Algorithms & Analysis,
-                Computer Architecture, Software Engineering, Human-Computer
-                Interactn(HCI), Web Development, Computer Networks, Machine
-                Learning, Robotics
-              </div>
+      <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+        Aug 2018 - May 2022
+      </div>
+      <div className="text-xl font-bold text-gray-900 dark:text-white font">
+        <a href="https://www.vrsiddhartha.ac.in/">
+          VELAGAPUDI RAMAKRISHDA SIDDHARTHA COLLEGE ⤴
+        </a>
+      </div>
+      <div className="text-lg font-semibold text-gray-700 dark:text-white">
+        Batchlers of Engineering
+      </div>
+        <div className="max-w-md mx-auto text-gray-500">
+          <strong className="text-gray-700">CourseWork : </strong> Data Structures & Algorithms, C, JAVA,
+          Machine Learning, Compilers
         </div>
-        </div>   
-);
+      
+    </div>
+  );
 }
