27e58ac8da2fc5c74be523116d8b7dddbfd54749
karthik <43676649+brkuhgk@users.noreply.github.com>
2024-03-30 12:06:31 -0400

    Updated Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,57 +23,79 @@ const projects = [
 export default function Projects() {
   return (
     <>
-      <div className="flex flex-wrap items-start justify-center px-4 py-2 bg-gray-100 rounded-lg shadow-md m-20">
-        <FlipCard projects={projects} />
-        <FlipCard projects={projects} />
-        <FlipCard projects={projects} />
-        <FlipCard projects={projects} />
-        <FlipCard projects={projects} />
-        <FlipCard projects={projects} />
+      <div class="flex justify-center">
+        <div class="border-2 border-gray-300 p-4 rounded-lg">
+          <h1 class="text-4xl font-bold m-0">Projects</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap items-start justify-center px-4 py-2 rounded-lg shadow-md m-10">
+        <Card projects={projects} />
+        <Card projects={projects} />
+        <Card projects={projects} />
+        <Card projects={projects} />
+        <Card projects={projects} />
+        <Card projects={projects} />
       </div>
     </>
   );
 }
 
-
-
 function FlipCard({ projects }) {
   return (
     <>
-      <section class="bg-grey-900 flex justify-center items-center text-white p-4">
-        <div class="w-[300px] h-[420px] bg-transparent cursor-pointer group perspective">
+      <div class="bg-grey-900 flex justify-center items-center">
+        <div class="w-[300px] h-[310px] bg-transparent cursor-pointer group perspective">
           <div class="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
-            <div class="absolute backface-hidden border-2 w-full h-full">
-              <img src={projects[0].image} alt="img" className="w-full h-full" />
+            <div class="absolute backface-hidden border-2 w-full h-auto">
+              <div class="flex justify-center">
+                <img
+                  src={projects[0].image}
+                  alt="Project-IMG"
+                  className="rounded-full
+            w-24 h-24 md:w-32 md:h-32 lg:w-48 lg:h-48"
+                />
+              </div>
+
+              <div className="font-mono p-2">
+                <div className="font-bold">Tech Stack </div> REACTJS, NODEJS,
+                MySQL,REACTJS, NODEJS, MySQL
+              </div>
             </div>
 
-            <div className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 overflow-hidden">
-              <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
-                <h1 className="text-3xl font-semibold"> {projects[0].name}</h1>
+            <div className="absolute my-rotate-y-180 backface-hidden w-full h-auto bg-gray-100 overflow-hidden">
+              <div className="text-center flex flex-col items-center justify-center h-auto text-gray-800 px-2 pb-24">
+                <h1 className="text-3xl font-semibold m-2">
+                  {projects[0].name}
+                </h1>
                 <p>{projects[0].desc}</p>
-
-                <div className="rounded-lg shadow-md font-mono m-2">
-                  <div className="font-bold">Tech Stack</div> REACTJS, NODEJS,
-                  MySQL,REACTJS, NODEJS, MySQL
-                </div>
-
-                <div className="flex space-x-14">
-                  <img
-                    src={live}
-                    alt="project logo"
-                    className="w-[50px] h-[50px]"
-                  />
-                  <img
-                    src={githubLogo}
-                    alt="project logo"
-                    className="w-[50px] h-[50px]"
-                  />
-                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </>
+  );
+}
+
+function Card({ projects }) {
+  return (
+    <>
+      <div className="bg-white rounded-lg shadow-md p-4 m-4">
+        <FlipCard projects={projects} />
+        <div className="flex justify-center space-x-14 p-4">
+          <a href="https://example.com" target="github" class="inline-block">
+            <img src={live} alt="project logo" className="w-[50px] h-[50px]" />
+          </a>
+          <a href="https://example.com" target="github" class="inline-block">
+            <img
+              src={githubLogo}
+              alt="project logo"
+              className="w-[50px] h-[50px]"
+            />
+          </a>
+        </div>
+      </div>
     </>
   );
 }
